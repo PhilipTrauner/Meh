9fe7c31f54c8273f199c6ce05d52e3396f902613
Philip Trauner <philip.trauner@aol.com>
2016-07-27 01:22:55 +0200

    Added for 2.7 compatibility

---
+++ Tests/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# This is here for Python 2.7 support.
+
+# ¯\_(ツ)_/¯
+"""
+A person shrugging their shoulders to indicate a lack of knowledge about a 
+particular topic, or a lack of care about the result of a situation.
+"""
