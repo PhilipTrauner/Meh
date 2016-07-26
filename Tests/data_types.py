da570008c104133c18bc1ad4005a06ed92bd4ee9
Philip Trauner <philip.trauner@aol.com>
2016-07-26 22:14:31 +0200

    Added real usage test

---
+++ Tests/data_types.py
@@ -0,0 +1,72 @@
+from os import remove
+from os.path import isfile
+from sys import path
+from os.path import abspath
+
+path.append(abspath("."))
+from Meh import Config, Option, UnsupportedDataTypeError
+
+CONFIG_PATH = "data_types.cfg"
+
+if isfile(CONFIG_PATH):
+	remove(CONFIG_PATH)
+
+config = Config()
+config.add(Option("list", [1, 2, 3]))
+config.add(Option("tuple", (1, 2, 3)))
+config.add(Option("dict", {"foo" : "baz", "test" : 123}))
+config.add(Option("bytes", b"test"))
+config.add(Option("string", "test"))
+config.add(Option("float", 42.0))
+config.add(Option("complex", (1+2j)))
+config.add(Option("int", 42))
+config.add(Option("boolean", False))
+
+try:
+	config = config.load(CONFIG_PATH)
+except IOError:
+	config.dump(CONFIG_PATH)
+	config = config.load(CONFIG_PATH)
+
+print(config.list)
+print(config.tuple)
+print(config.dict)
+print(config.bytes)
+print(config.string)
+print(config.float)
+print(config.complex)
+print(config.int)
+print(config.boolean)
+
+config.list = [1, 2, 3, 4]
+config.tuple = ("1", "2", "3")
+config.dict = {"foo" : 123, "test" : 124.0, "foo1" : "bar"}
+config.bytes = b"bytes"
+config.string = "string"
+config.float = 10.0
+config.complex = (2+2j)
+config.int = 43
+config.boolean = True
+
+try:
+	config.list = [1, 2, 3, [lambda x: True]]
+except UnsupportedDataTypeError:
+	print("Value validatation succeeded for list")
+
+try:
+	config.tuple = ("1", "2", (lambda x: True, ))
+except UnsupportedDataTypeError:
+	print("Value validatation succeeded for tuple")
+
+try:
+	config.dict = {"foo" : lambda x: True, "test" : 124.0, "foo1" : "bar"}
+except UnsupportedDataTypeError:
+	print("Value validatation succeeded for dict")
+
+print("File contents:")
+print(config._dumps())
+
+remove(CONFIG_PATH)
+if isfile(CONFIG_PATH + "c"):
+	remove(CONFIG_PATH + "c")
+	
