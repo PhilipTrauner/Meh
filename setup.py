1b284aa828d25daf8f56f67245de7b9ebf1cc0a2
Philip Trauner <philip.trauner@aol.com>
2017-11-12 00:52:21 +0100

    Included long_description, fixed email address and casing

--- setup.py
+++ setup.py
@@ -2,23 +2,35 @@
 import os
 from setuptools import setup, find_packages
 
-try:
-    from pypandoc import convert
-    read_md = lambda f: convert(f, "rst")
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, "r").read()
+long_description = """Meh
+===
+
+.. figure:: http://content.philip-trauner.me/meh.png
+   :alt: Meh Banner
+
+
+|Python version support: 2.7, 3.4, 3.5| |License: MIT|
+
+***Meh*** is a **Python configuration utility** that uses **Python** as
+it’s **data format**. It provides a pleasant and very “pythonic”
+interface to do all the things a configuration utility does, like
+default values, validators and comments.
+
+
+.. |Python version support: 2.7, 3.4, 3.5| image:: https://img.shields.io/badge/python-2.7%2C%203.4%2C%203.5-blue.svg
+.. |License: MIT| image:: https://img.shields.io/badge/license-MIT-blue.svg
+"""
 
 setup(
 	name="Meh",
-	version="1.0",
+	version="1.1",
 	author="Philip Trauner",
-	author_email="philip.trauner@aol.com",
+	author_email="philip.trauner@arztpraxis.io",
 	url="https://github.com/PhilipTrauner/Meh",
 	packages=find_packages(),
-	test_suite="Tests.tests.tests",
+	test_suite="tests.tests.tests",
 	description="Python configuration files in Python.",
-	long_description=read_md("README.md"),
+	long_description=long_description,
 	classifiers=[
 		"Development Status :: 5 - Production/Stable",
 		"Programming Language :: Python :: 2.7",
@@ -27,5 +39,5 @@ setup(
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: MIT License",
 	],
-	keywords=["config", "cfg", "meh", "py", "json"]
+	keywords=["config", "cfg", "meh", "py"]
 )
